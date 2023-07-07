2156abb997a6539790d66e3da7aa37e60aa04aaa
aaH6uos <107555762+aaH6uos@users.noreply.github.com>
2023-07-07 16:45:05 +0900

    [level 2] Title: 모음 사전, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.js
@@ -1,10 +1,4 @@
 function solution(word) {
-    // A로 시작하는 길이 1 -> 1
-    // A로 시작하는 길이 2 -> 5
-    // A로 시작하는 길이 3 -> 5**2
-    // A로 시작하는 길이 4 -> 5**3
-    // A로 시작하는 길이 5 -> 5**4
-    // A로 시작 단어 수 => (5**5-1)/4
     const charArr = ['A', 'E', 'I', 'O', 'U'];
     let answer = 0;
     for (let i=0; i<word.length; i++) {
