48c1ed9d5d7da72466d17c486d4236314f4aad21
aaH6uos <107555762+aaH6uos@users.noreply.github.com>
2023-07-07 16:06:45 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/81302. 거리두기 확인하기/거리두기 확인하기.js
@@ -0,0 +1,33 @@
+function solution(places) {
+    function check (arr) {
+        arr = arr.map(row => row.split(''));
+        for (let i=0; i<5; i++) {
+            for (let j=0; j<5; j++) {
+                // 인접한 P가 존재하는 경우 return 0
+                if (arr[i][j] == 'P') {
+                    if (j < 4 && arr[i][j+1] == 'P') return 0
+                    if (i < 4 && arr[i+1][j] == 'P') return 0                    
+                }
+                // O와 인접한 P가 2개 이상일 때 return 0
+                if (arr[i][j] == 'O') {
+                    let cnt = 0;
+                    if (j < 4 && arr[i][j+1] == 'P') cnt++
+                    if (i < 4 && arr[i+1][j] == 'P') {
+                        cnt++;
+                        if (cnt >= 2) return 0
+                    }
+                    if (j > 0 && arr[i][j-1] == 'P') {
+                        cnt++;
+                        if (cnt >= 2) return 0
+                    }
+                    if (i > 0 && arr[i-1][j] == 'P') {
+                        cnt++;
+                        if (cnt >= 2) return 0
+                    }
+                }
+            }
+        }
+        return 1
+    }
+    return places.map(check)
+}
