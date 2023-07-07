e11d92d1c3f2ec2e0498144b3e0565cf9e5417f2
aaH6uos <107555762+aaH6uos@users.noreply.github.com>
2023-07-07 12:29:54 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,12 +1,4 @@
 function solution(n) {
-    let answer = 0;
-    const arr = [];
-    while (n > 0) {
-        arr.push(n % 3)
-        n = Math.floor(n / 3)
-    }
-    arr.forEach((ele, idx) => {
-        answer += ele * (3 ** (arr.length - idx - 1))
-    })
-    return answer
+    const reversed =  [...n.toString(3)].reverse().join('')
+    return parseInt(reversed, 3)
 }
