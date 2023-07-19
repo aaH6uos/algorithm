18201d62de69553a1be706da43701af8f2b62a25
aaH6uos <107555762+aaH6uos@users.noreply.github.com>
2023-07-19 23:50:02 +0900

    [level 1] Title: 폰켓몬, Time: 0.22 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.js
@@ -1,8 +1,4 @@
 function solution(nums) {
-    const monMap = new Map();
-    for (let monNum of nums) {
-        let cnt = monMap.get(monNum)
-        cnt ? monMap.set(monNum, cnt + 1) : monMap.set(monNum, 1)
-    }
-    return nums.length / 2 < monMap.size ? nums.length / 2 : monMap.size;
+    const numCount = new Set(nums).size
+    return nums.length / 2 < numCount ? nums.length / 2 : numCount;
 }
