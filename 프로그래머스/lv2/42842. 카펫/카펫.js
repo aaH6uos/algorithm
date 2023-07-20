310ff89b3c84100b58fd5869488a9f73df26ab5b
aaH6uos <107555762+aaH6uos@users.noreply.github.com>
2023-07-20 22:08:31 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,10 +1,8 @@
 function solution(brown, yellow) {
-    // 노란색 약수 배열 -> 노란색의 number of row -> 가로가 긴 쪽 이므로 제곱근보다 작거나 같아야 함
     const arr = [];
     for (let i=1; i<=Math.sqrt(yellow); i++) {
         if (yellow % i == 0) arr.push(i)
     }
-    // 배열 돌면서 갈색 수와 맞는지 확인
     let answer = [];
     arr.forEach(num => {
         let row = num + 2, col = (yellow / num) + 2
