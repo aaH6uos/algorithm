800bfbcf1eeca52ad5aa17f3fe630d8f1fa61086
aaH6uos <107555762+aaH6uos@users.noreply.github.com>
2023-10-05 23:33:18 +0900

    [unrated] Title: 귤 고르기, Time: 503.09 ms, Memory: 44 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,28 @@
+function getCount(arr, map) {
+    arr.forEach((item) => {
+        if (map.has(item)) {
+            map.set(item, map.get(item)+1) 
+        } else {
+            map.set(item, 1)
+        }
+    })
+    return [...map.values()].sort((a, b) => b - a)
+}
+
+function getSum(arr) {
+    return arr.reduce((acc, cur) => acc + cur, 0)
+}
+
+function solution(k, tangerine) {
+    const tangMap = new Map();
+    const arr = getCount(tangerine, tangMap)
+    const ea = []
+
+    for (let i = 0; i <= arr.length; i++) {
+        ea.push(arr[i])
+
+        if (getSum(ea) >= k) break; 
+    }
+
+    return ea.length;
+}
